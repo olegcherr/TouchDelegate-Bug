610bfdeffcc9dd5df957623eea49fb11ab47ac85
Oleg Cherr <olegcherr@gmail.com>
2019-02-22 14:06:57 +0400

    Initial commit

---
+++ app/src/main/java/olegcherr/android/touchdelegatebug/MainActivity.kt
@@ -0,0 +1,67 @@
+package olegcherr.android.touchdelegatebug
+
+import android.graphics.Rect
+import android.os.Bundle
+import android.os.SystemClock
+import android.support.v7.app.AppCompatActivity
+import android.view.MotionEvent
+import android.view.TouchDelegate
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private val rootView get() = findViewById<ViewGroup>(R.id.rootView)
+    private val targetView get() = findViewById<View>(R.id.targetView)
+    private val logTextView get() = findViewById<TextView>(R.id.logTextView)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        targetView.setOnTouchListener { _, event ->
+            log("TARGET event=${event.name}")
+            true
+        }
+
+        rootView.setOnTouchListener { v, event ->
+            log("ROOT event=${event.name}")
+
+            if (!v.touchDelegate!!.onTouchEvent(event)) {
+                // Do something else
+            }
+
+            true
+        }
+
+        rootView.post {
+            val rect = Rect()
+            val inset = (-20).dp
+
+            targetView.getHitRect(rect)
+            rect.inset(inset, inset)
+            rootView.touchDelegate = TouchDelegate(rect, targetView)
+        }
+    }
+
+
+    private val Int.dp get() = Math.round(this * resources.displayMetrics.density)
+
+    private val MotionEvent.name: String?
+        get() {
+            return when (action) {
+                MotionEvent.ACTION_DOWN -> "ACTION_DOWN"
+                MotionEvent.ACTION_MOVE -> "ACTION_MOVE"
+                MotionEvent.ACTION_UP -> "ACTION_UP"
+                MotionEvent.ACTION_CANCEL -> "ACTION_CANCEL"
+                else -> null
+            }
+        }
+
+
+    private fun log(msg: String) {
+        logTextView.text = StringBuilder(logTextView.text).insert(0, "${SystemClock.uptimeMillis()} $msg\n")
+    }
+}
